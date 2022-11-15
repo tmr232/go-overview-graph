89f7bcd95916143ab0fab83fa341c0e4dfeed283
Tamir Bahar <tamir.bahar@gmail.com>
2022-11-15 21:31:01 +0200

    Basic export of entire package implemented.
    We have one major issue - Gonum graph doesn't allow for
    self-edges. So we'll need to use another package or defer
    the entire DOT generation to Python.

--- main_goat.go
+++ main_goat.go
@@ -12,7 +12,7 @@ func init() {
 			flags.MakeFlag[string]("pkg", "The path of the package to load.\nYou may need to run 'go get `package`' to fetch it first.", nil).AsCliFlag(),
 			flags.MakeFlag[string]("function", "The name of the function to generate an overview of.", nil).AsCliFlag(),
 		},
-		Name:  "GenerateOverview",
+		Name:  "function",
 		Usage: "creates a graph overview of the given function and\nprints it out in graphviz DOT format to STDOUT.",
 		Action: func(c *cli.Context) error {
 			return GenerateOverview(
@@ -24,6 +24,27 @@ func init() {
 			cflags := make(map[string]any)
 			cflags["pkg"] = flags.GetFlag[string](c, "pkg")
 			cflags["function"] = flags.GetFlag[string](c, "function")
+			return cflags
+		},
+	})
+
+	goat.Register(PackageOverview, goat.RunConfig{
+		Flags: []cli.Flag{
+			flags.MakeFlag[string]("pkg", "The path of the package to load.\nYou may need to run 'go get `package`' to fetch it first.", nil).AsCliFlag(),
+			flags.MakeFlag[string]("out", "Output file will be written to `path`.", nil).AsCliFlag(),
+		},
+		Name:  "package",
+		Usage: "generates an overview for an entire package.",
+		Action: func(c *cli.Context) error {
+			return PackageOverview(
+				flags.GetFlag[string](c, "pkg"),
+				flags.GetFlag[string](c, "out"),
+			)
+		},
+		CtxFlagBuilder: func(c *cli.Context) map[string]any {
+			cflags := make(map[string]any)
+			cflags["pkg"] = flags.GetFlag[string](c, "pkg")
+			cflags["out"] = flags.GetFlag[string](c, "out")
 			return cflags
 		},
 	})
